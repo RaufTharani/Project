e1bc4f4d525b3bc6132ae98bf1da565cce7989b9
RaufTharani <88101309+RaufTharani@users.noreply.github.com>
2021-07-29 12:41:57 -0400

    Add files via upload

---
+++ Project_Template_C9_Background_Color_Changer-main/sketch.js
@@ -0,0 +1,28 @@
+var button
+function setup() {
+  createCanvas(400, 400); 
+  button_red=createButton("RED")
+  button_red.position(200,60)
+  button_red.mousePressed(red_background)
+
+  button_green=createButton("GREEN")
+  button_green.position(350,60)
+  button_green.mousePressed(green_background)
+}
+
+function red_background() {
+r=255;
+g=0;
+b=0;
+}
+
+function green_background(){
+r=0;
+g=255;
+b=0;
+}
+
+drawSprites()
+
+
+
